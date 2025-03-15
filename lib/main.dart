42a7e5f64c66b181417e1984ce9a209d7477223b
Excel Asaph <e.asaph@alustudent.com>
2025-03-15 10:13:37 +0200

    Forgot Password

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
+import 'screens/log_in_screen.dart';
+import 'screens/create_account_screen.dart';
+import 'screens/verify_account_screen.dart';
+import 'screens/email_verified_screen.dart';
+import 'screens/forgot_password_screen.dart';
+// import 'package:inrida/screens/check_email_screen.dart';
+// import 'package:indrida/screens/password_reset_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +28,17 @@ class InRidaApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/login': (context) => LogInScreen(),
+        '/create_account': (context) => const CreateAccountScreen(),
+        '/verify_account': (context) => const VerifyAccountScreen(),
+        '/email_verified': (context) => const EmailVerifiedScreen(),
+        '/forgot_password': (context) => const ForgotPasswordScreen(),
+        // '/check_email': (context) => const CheckEmailScreen(),
+        // '/password_reset': (context) => const PasswordResetScreen(),
+      },
     );
   }
 }
